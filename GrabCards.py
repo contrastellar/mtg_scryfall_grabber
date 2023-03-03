86f180e04f5deb2d579d5b3833a59ce375eef3b0
Gabriella Agathon <gjstimac@mtu.edu>
2023-03-03 15:17:16 -0600

    Linted files manually.

--- GrabCards.py
+++ GrabCards.py
@@ -1,20 +1,29 @@
 #/opt/homebrew/bin/python3
+"""
+Utility script in order to pull complete card sets from Scryfall
 
-# Utility script in order to pull complete card sets from Scryfall
+Author: Contrastellar (Gabriella Agathon)
+"""
 
-import os, requests, json, argparse, time
+
+import json
+import time
+import requests
 
 def Merge(dict1, dict2):
-    return(dict1.update(dict2))
+    """Merge two dictionaries together"""
+    return dict1.update(dict2)
 
-# Return a string object of the json passed in via obj
 def jsonParse(obj):
+    """ Return a string object of the json passed in via obj"""
     text = json.dumps(obj, sort_keys=True, indent=3)
     return text
 
 # Returns the complete JSONList
 def GrabCards(UserSet, ResponseData, PageNum, VerboseSetting):
-
+    """
+        Grabs all cards from a user defined set @UserSet
+    """
     parsedCardFile = json.loads(jsonParse(ResponseData.json()))
 
     masterOutput = {}
@@ -24,7 +33,7 @@ def GrabCards(UserSet, ResponseData, PageNum, VerboseSetting):
         output[i+1] = d["name"]
         i = i + 1
         Merge(masterOutput, output)
-    
+
     # Sleeps are used to not get blocked by the API
     time.sleep(1)
 
@@ -36,9 +45,12 @@ def GrabCards(UserSet, ResponseData, PageNum, VerboseSetting):
         PageNum += 1
         cardListURL = "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3A"+ UserSet +"&unique=prints&page=" + str(PageNum)
 
-        responseData = requests.get(cardListURL)
-        if(VerboseSetting): print("Card URL =   " + str(cardListURL))
-        if(VerboseSetting): print("Response code: " + str(responseData.status_code) + "\n")
+        responseData = requests.get(cardListURL, timeout=10000)
+        if VerboseSetting:
+            print("Card URL =   " + str(cardListURL))
+        if VerboseSetting:
+            print("Response code: " + str(responseData.status_code) + "\n")
+
         parsedCardFile = json.loads(jsonParse(responseData.json()))
         hasNext = parsedCardFile['has_more']
         output2 = {}
--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 #/opt/homebrew/bin/python3
+"""
+Module for scraping usefull info off of the scryfall REST API
 
-import os, requests, json, argparse
+Author: Contrastellar (Gabriella Agathon)
+2023
+"""
+
+import os
+import json
+import argparse
+import requests
 import GrabCards
 
-def Merge(dict1, dict2):
-    return(dict1.update(dict2))
 
-# Return a string object of the json passed in via obj
 def jsonParse(obj):
+    """
+    Return a string object of the json passed in via obj
+    """
     text = json.dumps(obj, sort_keys=True, indent=3)
     return text
 
 def main():
+    """
+    main, allowing this to be invoked as a module from the command line.
+    """
 
     # Current dir the script is running in, can be useful for debugging
     script_dir = os.path.abspath(os.path.dirname( __file__ ))
 
     description = "Script to pull card info from api.scryfall.com based on specific set"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('set', metavar='set', help="3-5 letter/number ID that dictates which set to pull from") #set to pull card data from
+
+    #set to pull card data from
+    parser.add_argument('set', metavar='set', help="3-5 letter/number ID that dictates which set to pull from")
     parser.add_argument('-c', '--cards', help="grab card names associated with specific IDs", action='store_true')
     parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
 
@@ -29,26 +43,34 @@ def main():
     pullCardInfo = bool(args.cards)
     pageNum = 1
 
-    if(verboseSetting): print("Outputting verbosely\n"+script_dir)
+    if verboseSetting:
+        print("Outputting verbosely\n"+script_dir)
 
     # Used to verify that the two sets are actually identical.
     cardSetURL = "https://api.scryfall.com/sets/" + userSet
 
     cardListURL = "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3A"+ userSet +"&unique=prints&page=" + str(pageNum)
 
-    setData = requests.get(cardSetURL)
-    responseData = requests.get(cardListURL)
+    setData = requests.get(cardSetURL, timeout=10000)
+    responseData = requests.get(cardListURL, timeout=10000)
 
-    if(verboseSetting): print("Set URL  =   " + str(cardSetURL))
-    if(verboseSetting): print("Card URL =   " + str(cardListURL))
+    if verboseSetting:
+        print("Set URL  =   " + str(cardSetURL))
 
-    if(verboseSetting): print("Response code: " + str(responseData.status_code) + "\n")
+    if verboseSetting:
+        print("Card URL =   " + str(cardListURL))
+
+    if verboseSetting:
+        print("Response code: " + str(responseData.status_code) + "\n")
 
     parsedSetFile = json.loads(jsonParse(setData.json()))
     parsedCardFile = json.loads(jsonParse(responseData.json()))
 
-    if(verboseSetting): print("Card set from the set search ...  ? -> " + parsedSetFile['name'])
-    if(verboseSetting): print("Card set from the card ID    ... " + parsedCardFile["data"][0]["collector_number"] + "? -> "+ parsedCardFile['data'][0]['set_name'])
+    if verboseSetting:
+        print("Card set from the set search ...  ? -> " + parsedSetFile['name'])
+
+    if verboseSetting:
+        print("Card set from the card ID    ... " + parsedCardFile["data"][0]["collector_number"] + "? -> "+ parsedCardFile['data'][0]['set_name'])
 
     setNameFromSearch = parsedSetFile['name']
     setNameFromCard = parsedCardFile['data'][0]['set_name']
@@ -57,11 +79,11 @@ def main():
         print("Are strings the same?")
         if(setNameFromCard == setNameFromSearch):
             print("Yes!")
-            
+
         else:
             print("No! Exiting!")
             print("\n\n")
-            return -1
+            return
 
     """
         At this point, we can pretty much get on with it
@@ -76,9 +98,9 @@ def main():
         MasterOutput = GrabCards.GrabCards(UserSet=userSet, ResponseData=responseData, PageNum=pageNum, VerboseSetting=verboseSetting)
         # Serializing json
         outputObject = json.dumps(MasterOutput, indent=4)
-    
+
         # Writing to sample.json
-        with open("output/" + setNameFromSearch + "_card_name_info.json", "w") as outfile:
+        with open("output/" + setNameFromSearch + "_card_name_info.json", "w", encoding="UTF8") as outfile:
             outfile.write(outputObject)
 
 if __name__ == "__main__":
